e69a798faf693be5d0bc446e878de1b50833fa26
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-11-03 00:26:26 -0400

    Initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
