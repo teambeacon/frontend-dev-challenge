addb7003e7fbc1b0d3bb94df0647f1a83608385d
Leo Lad <87998262+Dedition@users.noreply.github.com>
2022-11-03 11:33:42 -0400

    Added state hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../styles/Home.module.css';
-import Image from 'next/image';
+import React, { useState, useEffect } from 'react'
+import styles from '../styles/Home.module.css'
+import Image from 'next/image'
+
 
 export default function Home() {
+  const api_url = 'https://api.sendbeacon.com/team/schools';
+  const [schools, setSchools] = useState([]);
+  const [location, setLocation] = useState(false);
+  const [userLocation, setUserLocation] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
+  const [hoveredSchool, setHoveredSchool] = useState(null);
+
+
+
   return (
     <div></div>
   )
