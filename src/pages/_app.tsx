fef7a57633202dfb56a398a18309229331c1c7db
femiorok <68767356+femiorok@users.noreply.github.com>
2022-11-03 20:31:18 -0700

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from 'next/app'
+import { store } from '../../lib/store'
+import { Provider } from 'react-redux'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Provider store={store}>
+            <Component {...pageProps} />
+         </Provider>
 }
+
